fdfa76d9196bb91bac80a40e2de67d42d45b10bf
Michael Nthiwa <mike.nthiwa@gmail.com>
2019-05-28 15:08:16 +0300

    Revert "ch(setting up folder structure):"

--- index.js
+++
@@ -1,9 +0,0 @@
-import http from 'http';
-import app from './src/App'
-import env from './src/environment/env';
-
-
-const server = http.createServer(app);
-server.listen(env.port, () => {
-  console.log(`I am listening to port ${env.port}`)
-});
